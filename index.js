d98e762cb2b77aa3f7b801bc7cd2dfa0878e6c3c
ankush789 <ankushsharma.4771@gmail.com>
2020-09-04 15:24:31 +0530

    setting response headers

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const fs=require('fs');
 //Serving a response to the browser --- a plain text
  function requestHandler(req, res){
      console.log(req.url);
-     res.end("HELLO!!");
+
+     //setting response header
+    res.writeHead(200,{'content-type':'text/html'});
+    res.end("<h1>Hello!!</h1>")
  }
 
  
