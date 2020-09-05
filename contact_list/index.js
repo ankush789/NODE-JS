ee5e3e50ffccd6ad8fde118863213cc7c39c6d67
ankush789 <ankushsharma.4771@gmail.com>
2020-09-05 22:06:24 +0530

    Rendering EJS file from views to browser

--- contact_list/index.js
+++ contact_list/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const path = require('path');
 const port = 8800;
 
 const app = express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    
+    return res.render('home');
+
+});
+
 
 app.listen(port, (err)=>{
     if(err){
