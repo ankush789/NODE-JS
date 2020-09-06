68e61565fa8a995ce1a09cb7c28344dbeccbd03d
ankush789 <ankushsharma.4771@gmail.com>
2020-09-06 16:19:58 +0530

    using loops and conditional loops

--- contact_list/index.js
+++ contact_list/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { get } = require('http');
 const port = 8800;
 
 const app = express();
@@ -8,9 +9,10 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.get('/',(req,res)=>{
-    
     return res.render('home', {title: "ANKUSH"});
-
+});
+app.get('/practice',(req,res)=>{
+    return res.render('practice');
 });
 
 
